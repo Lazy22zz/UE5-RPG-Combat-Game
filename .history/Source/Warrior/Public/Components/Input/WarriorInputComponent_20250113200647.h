40b6ceddb90ad74f8e372957f74db2b6ecf9d597
Leon Ye <141181283+Lazy22zz@users.noreply.github.com>
2025-01-13 20:52:20 -0800

    create inputcomponent
    Purpose: Define input mapping method.

---
+++ .history/Source/Warrior/Public/Components/Input/WarriorInputComponent_20250113200647.h
@@ -0,0 +1,17 @@
+// ALL FREE
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "WarriorInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UWarriorInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Warrior/Private/Components/Input/WarriorInputComponent.cpp
@@ -0,0 +1,5 @@
+// ALL FREE
+
+
+#include "Components/Input/WarriorInputComponent.h"
+
