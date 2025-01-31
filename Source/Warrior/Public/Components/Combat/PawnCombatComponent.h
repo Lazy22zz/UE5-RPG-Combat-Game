ed2137affa7c336dbc8d1ba794e99c96d7ecfb7d
Leon Ye <141181283+Lazy22zz@users.noreply.github.com>
2025-01-31 11:38:46 -0800

    registered weapons with its tag

--- Source/Warrior/Public/Components/Combat/PawnCombatComponent.h
+++ Source/Warrior/Public/Components/Combat/PawnCombatComponent.h
@@ -4,8 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "Components/PawnExtenComponentBase.h"
+#include "NativeGameplayTags.h"
 #include "PawnCombatComponent.generated.h"
 
+class AWarriorWeaponBase;
 /**
  * 
  */
@@ -13,5 +15,21 @@ UCLASS()
 class WARRIOR_API UPawnCombatComponent : public UPawnExtenComponentBase
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = Category = "Warrior|Combat")
+	void RegisterSpawnedWeapon(FGameplayTag InWeaponTagToRegister, AWarriorWeaponBase* InWeaponToRegister, bool bRegisterAsEquippedWeapon = false); // fasle for hero, true for enemy
+
+	UFUNCTION(BlueprintCallable, Category = "Warrior|Combat")
+	AWarriorWeaponBase* GetCharacterCarriedWeaponByTag(FGameplayTag InWeaponTagToGet) const;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Warrior|Combat")
+	FGameplayTag CurrentEquippedWeaponTag;
+
+	UFUNCTION(BlueprintCallable, Category = "Warrior|Combat")
+	AWarriorWeaponBase* GetCharacterCurrentEquippedWeapon() const;
+
+private:
+	TMap< FGameplayTag, AWarriorWeaponBase* >CharacterCarriedWeaponMap;
 	
 };
