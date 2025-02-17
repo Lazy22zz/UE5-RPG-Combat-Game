1a58b5e0c04e60b2cee8423e57f5fff9870742f7
Leon Ye <141181283+Lazy22zz@users.noreply.github.com>
2025-02-16 18:31:38 -0800

    grant equipeed axe ability

--- Source/Warrior/Public/AbilitySystem/WarriorAbilitySystemComponent.h
+++ Source/Warrior/Public/AbilitySystem/WarriorAbilitySystemComponent.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AbilitySystemComponent.h"
+#include "WarriorTypes/WarriorStructTypes.h"
 #include "WarriorAbilitySystemComponent.generated.h"
 
 /**
@@ -18,4 +19,6 @@ public:
 	void OnAbilityInputPressed(const FGameplayTag& InInputTag);
 	void OnAbilityInputReleased(const FGameplayTag& InInputTag);
 	
+	UFUNCTION(BlueprintCallable, Category = "Warrior|Ability", meta = (ApplyLevel = "1"))
+	void GrantHeroWeaponAbilities(const TArray<FWarriorHeroAbilitySet>& InDefaultWeaponAbilities, int32 ApplyLevel, TArray<FGameplayAbilitySpecHandle>& OutGrantedAbilitySpecHandles);
 };
