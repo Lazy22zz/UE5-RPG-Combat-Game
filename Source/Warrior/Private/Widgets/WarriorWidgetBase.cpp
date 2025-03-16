6e9f7c381170ec3c3cefe4fc09fa901e17ca0f36
Leon Ye <141181283+Lazy22zz@users.noreply.github.com>
2025-03-16 13:53:25 -0700

    Listen For Boardcast

---
+++ Source/Warrior/Private/Widgets/WarriorWidgetBase.cpp
@@ -0,0 +1,19 @@
+// ALL FREE
+
+
+#include "Widgets/WarriorWidgetBase.h"
+#include "Interfaces/PawnUIInterface.h"
+
+void UWarriorWidgetBase::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	if (IPawnUIInterface* PawnUIInterface = Cast<IPawnUIInterface>(GetOwningPlayerPawn()))
+	{
+		if (UHeroUIComponent* HeroUIComponent = PawnUIInterface->GetHeroUIComponent())
+		{
+			BP_OnOwningHeroUIComponentInitialized(HeroUIComponent);
+		}
+	}
+}
+
