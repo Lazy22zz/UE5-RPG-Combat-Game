828d4b72d07d485daa17504cbae49d1067221c83
Leon Ye <141181283+Lazy22zz@users.noreply.github.com>
2025-04-01 10:30:14 -0700

    Crowd Follong Component

---
+++ Source/Warrior/Private/GameControllers/WarriorAIController.cpp
@@ -0,0 +1,16 @@
+// ALL FREE
+
+
+#include "GameControllers/WarriorAIController.h"
+#include "Navigation/CrowdFollowingComponent.h"
+
+#include "WarriorDebugHelper.h"
+
+AWarriorAIController::AWarriorAIController(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer.SetDefaultSubobjectClass<UCrowdFollowingComponent>("PathFollowingComponent"))
+{
+	if (UCrowdFollowingComponent* CrowdComp = Cast<UCrowdFollowingComponent>(GetPathFollowingComponent()))
+	{
+		Debug::Print(TEXT("CrowdFollowingComponent valid"), FColor::Green);
+	}
+}
