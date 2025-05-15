d501fd3484990830dc39c0f08d4f560f8222e3a7
Leon Ye <141181283+Lazy22zz@users.noreply.github.com>
2025-05-15 10:05:10 -0700

    New Mapping Context

--- Source/Warrior/Public/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.h
+++ Source/Warrior/Public/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.h
@@ -7,6 +7,7 @@
 #include "HeroGameplayAbility_TargetLock.generated.h"
 
 class UWarriorWidgetBase;
+class UInputMappingContext;
 
 /**
  * 
@@ -32,10 +33,12 @@ private:
 	void DrawTargetLockWidget();
 	void SetTargetLockWidgetPosition();
 	void InitTargetLockMovement();
+	void InitTargetLockMappingContext();
 
 	void CancelTargetLockAbility();
 	void CleanUp();
 	void ResetTargetLockMovement();
+	void ResetTargetLockMappingContext();
 
 	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
 	float BoxTraceDistance = 5000.f;
@@ -57,6 +60,9 @@ private:
 
 	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
 	float TargetLockMaxWalkSpeed = 100.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	UInputMappingContext* TargetLockMappingContext;
 
 	UPROPERTY()
 	TArray<AActor*> AvailableActorsToLock;
