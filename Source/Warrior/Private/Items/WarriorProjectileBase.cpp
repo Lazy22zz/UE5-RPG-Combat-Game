489f635cc94c965c3a85ff1d1fd04760e0b1b956
Leon Ye <141181283+Lazy22zz@users.noreply.github.com>
2025-05-26 18:42:41 -0700

    Handle Projectile Hit If Is Blocking

--- Source/Warrior/Private/Items/WarriorProjectileBase.cpp
+++ Source/Warrior/Private/Items/WarriorProjectileBase.cpp
@@ -5,6 +5,9 @@
 #include "Components/BoxComponent.h"
 #include "NiagaraComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "WarriorFunctionLibrary.h"
+#include "WarriorGameplayTags.h"
+#include "AbilitySystemBlueprintLibrary.h"
 
 #include "WarriorDebugHelper.h"
 
@@ -47,16 +50,50 @@ void AWarriorProjectileBase::BeginPlay()
 
 void AWarriorProjectileBase::OnProjectileHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
-	if (OtherActor)
-	{
-		Debug::Print(OtherActor->GetActorNameOrLabel());
+	BP_OnSpawnProjectileHitFX(Hit.ImpactPoint);
 
+	APawn* HitPawn = Cast<APawn>(OtherActor);
+
+	if (!HitPawn || !UWarriorFunctionLibrary::IsTargetPawnHostile(GetInstigator(), HitPawn))
+	{
 		Destroy();
+		return;
 	}
+
+	bool bIsValidBlock = false;
+
+	const bool bIsPlayerBlocking = UWarriorFunctionLibrary::NativeDoesActorHaveTag(HitPawn, WarriorGameplayTags::Player_Status_Blocking);
+	
+	if (bIsPlayerBlocking)
+	{
+		bIsValidBlock = UWarriorFunctionLibrary::IsValidBlock(this, HitPawn);
+	}
+
+	FGameplayEventData Data;
+	Data.Instigator = this;
+	Data.Target = HitPawn;
+
+	if (bIsValidBlock)
+	{
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+			HitPawn,
+			WarriorGameplayTags::Player_Event_SuccessfulBlock,
+			Data
+		);
+		
+	}
+	else
+	{
+		//Apply projectile damage 
+	}
+
+	Destroy();
 }
 
 void AWarriorProjectileBase::OnProjectileBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 }
+
+
 
 
