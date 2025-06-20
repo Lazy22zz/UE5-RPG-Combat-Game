a2f0672d953e086dc8832d0c5effe3015242568f
Leon Ye <141181283+Lazy22zz@users.noreply.github.com>
2025-06-19 20:15:13 -0700

    Remove Boss Bar

--- Source/Warrior/Private/Components/UI/EnemyUIComponent.cpp
+++ Source/Warrior/Private/Components/UI/EnemyUIComponent.cpp
@@ -2,4 +2,31 @@
 
 
 #include "Components/UI/EnemyUIComponent.h"
+#include "Widgets/WarriorWidgetBase.h"
 
+void UEnemyUIComponent::RegisteredDrawnWidget(UWarriorWidgetBase* InWidgetToRegister)
+{
+	if (InWidgetToRegister)
+	{
+		EnemyDrawnWidgets.Add(InWidgetToRegister);
+	}
+
+	return;
+}
+
+void UEnemyUIComponent::RemoveAnyEnemyWidgetsIfAny()
+{
+	if (EnemyDrawnWidgets.IsEmpty())
+	{
+		return;
+	}
+
+	for (UWarriorWidgetBase* DrawnWidgets : EnemyDrawnWidgets)
+	{
+		if (DrawnWidgets)
+		{
+			DrawnWidgets->RemoveFromParent();
+		}
+	}
+
+}
