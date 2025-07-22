3cdb7c484b34a7fa78c556148696c1374dc5b043
Leon Ye <141181283+Lazy22zz@users.noreply.github.com>
2025-07-22 08:50:29 -0700

    Create Tags and create GA_Hero_Rage

--- Source/Warrior/Private/AbilitySystem/WarriorAbilitySystemComponent.cpp
+++ Source/Warrior/Private/AbilitySystem/WarriorAbilitySystemComponent.cpp
@@ -15,21 +15,10 @@ void UWarriorAbilitySystemComponent::OnAbilityInputPressed(const FGameplayTag& I
 	{
 		if (!AbilitySpec.DynamicAbilityTags.HasTagExact(InInputTag)) continue;
 		
-		if (InInputTag.MatchesTag(WarriorGameplayTags::InputTag_Toggleable))
-		{
-			if (AbilitySpec.IsActive())
-			{
-				CancelAbilityHandle(AbilitySpec.Handle);
-			}
-			else
-			{
-				TryActivateAbility(AbilitySpec.Handle);
-			}
-		}
+		if (InInputTag.MatchesTag(WarriorGameplayTags::InputTag_Toggleable) && AbilitySpec.IsActive())
+			CancelAbilityHandle(AbilitySpec.Handle);
 		else
-		{
 			TryActivateAbility(AbilitySpec.Handle);
-		}
 	}
 }
 
