835b8db8029f39756e417f2643435d9e80f4e747
Lazy22zz <claudeleave@gmail.com>
2025-09-17 14:47:30 -0700

    OnStoneBeginOverlapDone

--- Source/Warrior/Private/Items/Pickups/WarriorStoneBase.cpp
+++ Source/Warrior/Private/Items/Pickups/WarriorStoneBase.cpp
@@ -2,4 +2,14 @@
 
 
 #include "Items/Pickups/WarriorStoneBase.h"
+#include "Characters/WarriorHeroCharacter.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
+#include "WarriorGameplayTags.h"
 
+void AWarriorStoneBase::OnPickUpCollisionSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (AWarriorHeroCharacter* OverlappedCharacter = Cast<AWarriorHeroCharacter>(OtherActor))
+	{
+		OverlappedCharacter->GetWarriorAbilitySystemComponent()->TryActivateAbilityByTag(WarriorGameplayTags::Player_Ability_Pickup_Stones);
+	}
+}
