558d95d950b205f9dd502daf4f0aac5fd0b985a8
Lazy22zz <claudeleave@gmail.com>
2025-09-17 15:40:01 -0700

    CollectStones

--- Source/Warrior/Private/AbilitySystem/Abilities/HeroGameplayAbility_PickupStones.cpp
+++ Source/Warrior/Private/AbilitySystem/Abilities/HeroGameplayAbility_PickupStones.cpp
@@ -2,6 +2,9 @@
 
 
 #include "AbilitySystem/Abilities/HeroGameplayAbility_PickupStones.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Characters/WarriorHeroCharacter.h"
+#include "Items/PickUps/WarriorStoneBase.h"
 
 void UHeroGameplayAbility_PickupStones::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
@@ -11,4 +14,38 @@ void UHeroGameplayAbility_PickupStones::ActivateAbility(const FGameplayAbilitySp
 void UHeroGameplayAbility_PickupStones::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
 {
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UHeroGameplayAbility_PickupStones::CollectStones()
+{
+	CollectedStones.Empty();
+
+	TArray<FHitResult> TraceHits;
+
+	UKismetSystemLibrary::BoxTraceMultiForObjects(
+		GetHeroCharacterFromActorInfo(),
+		GetHeroCharacterFromActorInfo()->GetActorLocation(),
+		GetHeroCharacterFromActorInfo()->GetActorLocation() + -GetHeroCharacterFromActorInfo()->GetActorUpVector() * BoxTraceDistance,
+		TraceBoxSize / 2.f,
+		(-GetHeroCharacterFromActorInfo()->GetActorUpVector()).ToOrientationRotator(),
+		StoneTraceChannel,
+		false,
+		TArray<AActor*>(),
+		bDrawDebugShape ? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None,
+		TraceHits,
+		true
+	);
+
+	for (const FHitResult& TraceHit : TraceHits)
+	{
+		if (AWarriorStoneBase* FoundStone = Cast<AWarriorStoneBase>(TraceHit.GetActor()))
+		{
+			CollectedStones.AddUnique(FoundStone);
+		}
+	}
+
+	if (CollectedStones.IsEmpty())
+	{
+		CancelAbility(GetCurrentAbilitySpecHandle(), GetCurrentActorInfo(), GetCurrentActivationInfo(), true);
+	}
 }
