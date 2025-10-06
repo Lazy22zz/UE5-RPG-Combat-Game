5f8eb16938175292fe35f4234128e1481439aa2e
Lazy22zz <claudeleave@gmail.com>
2025-10-06 11:06:48 -0700

    TrySpawnWaveEnemiesDone

--- Source/Warrior/Public/GameModes/WarriorSurvialGamemode.h
+++ Source/Warrior/Public/GameModes/WarriorSurvialGamemode.h
@@ -64,7 +64,8 @@ private:
 	bool HasFinishedAllWaves() const;
 	void PreLoadNextWaveEnemies();
 	FWarriorEnemyWaveSpawnerTableRow* GetCurrentWaveSpawnerTableRow() const;
-
+	int32 TrySpawnWaveEnemiesNum();
+	bool ShouldKeepSpawnEnemies() const;
 
 	UPROPERTY()
 	EWarriorSurvialGameModeState CurrentSurvialGameModeState;
@@ -80,6 +81,15 @@ private:
 
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "WaveDefinition", meta = (AllowPrivateAccess = "true"))
 	int32 CurrentWaveCount = 1;
+
+	UPROPERTY()
+	int32 CurrentSpawnedEnemiesCounter = 0;
+
+	UPROPERTY()
+	int32 TotalSpawnedEnemiesThisWaveCounter = 0;
+
+	UPROPERTY()
+	TArray<AActor*> TargetPointsArray;
 
 	UPROPERTY()
 	float TimePassedSinceStart = 0.f;
